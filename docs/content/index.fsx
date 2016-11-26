7d56be31557e2a7a053bb66f0d3caa5af4ec79c6
rflechner <rflechner@romcyber.com>
2016-11-26 17:53:28 +0100

    TASK: using MyGet in documentation

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -12,7 +12,7 @@ LinqToSalesforce
 
 Documentation
 
-<div class="row">
+<!--div class="row">
   <div class="span1"></div>
   <div class="span6">
     <div class="well well-small" id="nuget">
@@ -21,7 +21,21 @@ Documentation
     </div>
   </div>
   <div class="span1"></div>
+</div-->
+
+
+<div class="row">
+  <div class="span1"></div>
+  <div class="span6">
+    <div class="well well-small" id="nuget">
+      The LinqToSalesforce library can be <a href="https://www.myget.org/Package/Details/romcyber?packageType=nuget&packageId=LinqToSalesforce">installed from MyGet</a>:
+      <pre>PM> Install-Package LinqToSalesforce</pre>
+    </div>
+  </div>
+  <div class="span1"></div>
 </div>
+
+It will be pushed on NuGet.org after more tests and some missing features implementations.
 
 Example
 -------
