702ea26f0c67aac1150f6fc934540b03b96b5532
rflechner <rflechner@romcyber.com>
2017-03-23 22:38:39 +0100

    DOC: update doc

--- src/LinqToSalesforce/AssemblyInfo.fs
+++ src/LinqToSalesforce/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("LinqToSalesforce")>]
 [<assembly: AssemblyProductAttribute("LinqToSalesforce")>]
 [<assembly: AssemblyDescriptionAttribute("Linq provider for Salesforce SOQL REST API")>]
-[<assembly: AssemblyVersionAttribute("0.2.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
+[<assembly: AssemblyVersionAttribute("0.2.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "LinqToSalesforce"
     let [<Literal>] AssemblyProduct = "LinqToSalesforce"
     let [<Literal>] AssemblyDescription = "Linq provider for Salesforce SOQL REST API"
-    let [<Literal>] AssemblyVersion = "0.2.0"
-    let [<Literal>] AssemblyFileVersion = "0.2.0"
+    let [<Literal>] AssemblyVersion = "0.2.1"
+    let [<Literal>] AssemblyFileVersion = "0.2.1"
